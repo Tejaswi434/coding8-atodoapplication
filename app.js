4ecff88aa6a76388d3b0b6511f00cf9b185e82ac
Tejaswi434 <tejatango301203@gmail.com>
2023-10-21 17:47:40 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "moviesData.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+app.get("/todos/", async (request, response) => {
+  const { status } = request.query;
+  const data = `select * from todo where status='${status}';`;
+  const sending = await db.get(data);
+  response.send(sending);
+});
